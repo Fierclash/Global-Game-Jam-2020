5acc8ed3d7c01049ad7170e9dcc4032515ce327d
benlf1 <54038475+benlf1@users.noreply.github.com>
2020-02-01 17:05:15 -0800

    Use 2DRogueLike Assets

---
+++ Global Game Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager boardManager;
+
+    void Awake()
+    {
+        boardManager = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame() {
+        boardManager.SetupScene();
+    }
+}
