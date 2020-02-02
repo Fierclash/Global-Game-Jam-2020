1ce58fe684fbc100fa150d3af846a384ce4b4811
KingLullaby <60530467+KingLullaby@users.noreply.github.com>
2020-02-02 14:42:00 -0800

    Level Reset
    When the player dies they're given the option to restart or exit, exit isn't implemented yet and restart starts the new level but nothing happens

---
+++ Global Game Jam/Assets/LossScreenScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LossScreenScript : MonoBehaviour
+{
+    private Scene menuScene, currentScene;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+
+    }
+
+    public void ResetGame()
+    {
+        Application.LoadLevel(currentScene.name);
+    }
+
+    public void LeaveLevel()
+    {
+        Application.LoadLevel(menuScene.name);
+    }
+
+}
