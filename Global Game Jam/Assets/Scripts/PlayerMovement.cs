cba63561bc50942085663343b9231d53c60172bb
Fierclash <49929727+Fierclash@users.noreply.github.com>
2020-02-01 18:18:59 -0800

    Player Can Actually Move
    suprising

---
+++ Global Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private bool canMove = true;
+
+	void Update()
+	{
+		if(Input.anyKey && canMove)
+		{
+			Move(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0));
+		}
+		else if(!Input.anyKey)
+		{
+			canMove = true;
+		}
+	}
+
+	void Move(Vector3 newPosition)
+	{
+		transform.position = transform.position + newPosition;
+		canMove = false;
+	}
+}
