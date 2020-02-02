269eb7905c1b0633777bac4a03827917ebad3b82
benlf1 <54038475+benlf1@users.noreply.github.com>
2020-02-01 17:55:51 -0800

    Add Player Script

---
+++ Global Game Jam/Assets/Scripts/Hole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Sprite built;					//Alternate sprite to display after Hole has been attacked by player.
+    public int hp = 0;							//hit points for the hole.
+    private SpriteRenderer spriteRenderer;		//Store a component reference to the attached SpriteRenderer.
+
+    void Start()
+    {
+        //Get a component reference to the SpriteRenderer.
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    //Rebuild is called when the player rebuilds a hole.
+    public void Rebuild (int loss)
+    {        
+        //Set spriteRenderer to the damaged wall sprite.
+        spriteRenderer.sprite = built;
+        
+        //Subtract loss from hit point total.
+        hp += loss;
+        
+        //If hit points are less than or equal to zero:
+        if(hp > 0)
+            //Disable the gameObject.
+            gameObject.SetActive (false);
+    }
+}
